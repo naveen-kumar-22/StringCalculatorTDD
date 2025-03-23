f5b485ed4316729533acbb4e5fab9aff7c017bd3
Naveen Kumar <naveen.kumar@anchanto.com>
2025-03-23 22:52:47 +0530

    handle single number

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -9,4 +9,8 @@ class StringCalculatorTest < Minitest::Test
   def test_add_empty_string_returns_zero
     assert_equal 0, @calculator.add("")
   end
+  
+  def test_add_single_number_returns_number
+    assert_equal 1, @calculator.add("1")
+  end
 end
