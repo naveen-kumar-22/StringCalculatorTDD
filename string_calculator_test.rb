c916b752aee3f94fdb45eab6c11009757d1fb602
Naveen Kumar <naveen.kumar@anchanto.com>
2025-03-23 23:07:51 +0530

    handle two numbers

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -9,8 +9,12 @@ class StringCalculatorTest < Minitest::Test
   def test_add_empty_string_returns_zero
     assert_equal 0, @calculator.add("")
   end
-  
+
   def test_add_single_number_returns_number
     assert_equal 1, @calculator.add("1")
+  end
+
+  def test_add_two_numbers_returns_sum
+    assert_equal 3, @calculator.add("1,2")
   end
 end
